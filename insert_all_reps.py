3c251a64a044082bd400b04eb8a567955a46d1e8
tim woods <tim.woods.tw@gmail.com>
2018-02-07 15:24:14 -0800

    Make insert_all_reps callable from other scripts.

--- insert_all_reps.py
+++ insert_all_reps.py
@@ -8,8 +8,22 @@ PASSWORD = 'aImFB83ClwKyoolJ'
 DB_NAME = 'VOTING'
 
 
-def insert_representative_into_table(rep):
-    global conn
+def clean_fetched_ID(id):
+    """Remove MySQL artifacts from fetched IDs"""
+    return ''.join([x for x in id if x not in "',"])
+
+
+def select_existing_representatives(connection):
+    """From the existing MySQL database, select all the existing representative IDs,
+    and return them as a set."""
+    cur = connection.cursor()
+    cur.execute("SELECT Rep_ID FROM Representative;")
+    rep_ids_punc = set(cur.fetchall())
+    rep_ids = set([clean_fetched_ID(x) for x in rep_ids_punc])
+    return rep_ids
+
+
+def insert_representative_into_table(rep, conn):
     cur = conn.cursor()
     updated = cur.execute(
         "INSERT IGNORE INTO Representative VALUES (%s, %s, %s, %s);",
@@ -18,6 +32,10 @@ def insert_representative_into_table(rep):
 
 
 def update_representatives_table(json_filepath):
+    global already_seen
+    conn = pymysql.connect(host=HOSTNAME, user=USERNAME, passwd=PASSWORD, db=DB_NAME)
+    already_seen = select_existing_representatives(conn)
+
     with open(json_filepath, 'r') as j_file:
         vote_object = json.load(j_file)
 
@@ -25,15 +43,7 @@ def update_representatives_table(json_filepath):
     for vote_result in every_vote.keys():
         for representative in every_vote[vote_result]:
             if representative['id'] not in already_seen:
-                insert_representative_into_table(representative)
+                insert_representative_into_table(representative, conn)
 
-
-def main():
-    global conn
-    conn = pymysql.connect(host=HOSTNAME, user=USERNAME, passwd=PASSWORD, db=DB_NAME)
-    update_representatives_table('sample_data.json')
     conn.commit()
     conn.close()
-
-if __name__ == '__main__':
-    main()
