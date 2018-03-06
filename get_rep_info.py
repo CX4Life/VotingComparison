5dff6ed201152ff78c9ecb21c8cf3ee458aef5dd
leea51 <leea51@wwu.edu>
2018-03-05 18:54:29 -0800

    "cleaned up get_rep_info.py"

--- get_rep_info.py
+++ get_rep_info.py
@@ -1,13 +1,3 @@
-'''
-TERMS
-repid
-type
-start
-end
-state
-district (null if sen)
-'''
-import json
 from datetime import date, datetime
 from get_bill import json_loader, json_dump
 import operator
@@ -41,84 +31,6 @@ def print_changed_districts(filepath):
                             print("%s %s %s changed district" % (
                                 x['id']['bioguide'], x['name']['first'], x['name']['last'],))
                         first = False
-
-
-def print_recent_districts(filepath):
-    data = json_loader(filepath)
-
-    for x in data:
-        print("%s %s %s" % (x['id']['bioguide'], x['name']['first'], x['name']['last'],))
-        for term in x['terms']:
-            if term['type'] == 'rep':
-                if term['start'] > date(2007, 12, 5).isoformat():
-                    print("\t%s: %s %s %s district %s" % (
-                        term['type'], term['start'], term['end'], term['state'], term['district']))
-
-
-# not deleting terms
-def trim(filepath):
-    data = json_loader(filepath)
-
-    for rep in data:
-        # district = 0
-        # first = True
-        terms = rep['terms']
-        for term in terms:
-            # if term['type'] == 'rep':
-            '''
-            if first:
-                district = term['district']
-                first = False
-            elif district != term['district']:
-                district = term['district']
-            '''
-            date = datetime.strptime(term['start'], "%Y-%m-%d")
-            if date < datetime(2007, 12, 5):
-                #print ("start %s" % term['start'])
-                del term
-
-    json_dump("dumpfile.json", data)
-
-'''
-class RepTerm:
-
-    def __init__(self, start_date, end_date, district, state):
-        self.start_date = start_date
-        self.end_date = end_date
-        self.district = district
-        self.state = state
-
-
-def get_district(rep_id, date):
-    historic = 'rep_info/legislators-historical.json'
-    current = 'rep_info/legislators-current.json'
-
-
-
-    data = json_loader(current)
-
-    termlist = [2][]
-    for x in data:
-        for term in x['terms']:
-            if term['type'] == 'rep':
-                # date = datetime.strptime(term['start'], "%Y-%m-%d")
-                if term['start'] > date(2012, 12, 25).isoformat():
-                    return term.get('district')
-
-    return termlist
-'''
-
-'''
-repID:
-    {
-        Entity:
-            {
-                'salience' = 0
-                'score' = 0
-                'magnitude' = 0
-            }
-    }
-'''
 
 
 def get_district(filepath, repID):
