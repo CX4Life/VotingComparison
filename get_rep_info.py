1841b1f126a56b1908d57c5f188c94326e99cea0
leea51 <leea51@wwu.edu>
2018-03-05 19:29:55 -0800

    "added districts.json"

--- get_rep_info.py
+++ get_rep_info.py
@@ -38,6 +38,22 @@ def print_changed_districts():
                         first = False
 
 
+
+def get_districts():
+    data = json_loader(file)
+
+    districts = {}
+
+    for x in data:
+        id = x['id']
+        repID = id['bioguide']
+        state, district = get_district(repID)
+        if district is not None:
+            district = str(district).rjust(2, '0')
+            districts[repID] = state + district
+    json_dump('districts.json', districts)
+
+
 def get_district(repID):
     data = json_loader(file)
 
@@ -50,18 +66,22 @@ def get_district(repID):
                 if term['type'] == 'rep':
                     if term['start'] > date(2013, 1, 3).isoformat():
                         if term['district'] not in districts:
-                            districts.append([term['start'], term['district']])
-            #print(districts)
+                            districts.append([term['start'], term['state'], term['district']])
             # returns the most district for the rep
-            index, last = max(enumerate(districts), key=operator.itemgetter(0))
-            return last[1]
+            if districts:
+                index, last = max(enumerate(districts), key=operator.itemgetter(0))
+                return last[1], last[2]
+            else:
+                #print("Error: shouldn't reach here")
+                return None, None
 
 
 def main():
-    print_changed_districts()
+    #print_changed_districts()
     #trim(current)
 
-    get_district("H001065")
+    #get_district("L000560")
+    get_districts()
 
 
 if __name__ == "__main__":
